b5bee21cb0d0dc80a11185ff365d7fb681dbbf50
akash arora <akasharoraakasharora377@gmail.com>
2021-07-22 15:22:24 +0530

    set env vars

--- devmeet/settings.py
+++ devmeet/settings.py
@@ -160,8 +160,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'akasharoraakasharora377@gmail.com'
-EMAIL_HOST_PASSWORD = 'twpzbppljpshezyl'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 
