4859ec25e099b52c7b0bed0d83bd6aa0286d2598
Akash <akasharoraakasharora377@gmail.com>
2022-09-27 16:50:32 +0530

    Reset

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 //GET SEARCH FORM AND PAGE LINKS
 let searchForm = document.getElementById('searchForm')
@@ -40,4 +41,48 @@ for(let i = 0; tags.length > i; i++){
             })
 
     })
+=======
+
+//GET SEARCH FORM AND PAGE LINKS
+let searchForm = document.getElementById('searchForm')
+let pageLinks = document.getElementsByClassName('page-link')
+
+//ENSURE SEARCH FORM EXISTS
+if(searchForm) {
+    for(let i=0; pageLinks.length > i; i++) {
+        pageLinks[i].addEventListener('click', function (e) {
+            e.preventDefault()
+
+            //GET THE DATA ATTRIBUTE
+            let page = this.dataset.page
+
+            //ADD HIDDEN SEARCH INPUT TO FORM
+            searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
+
+            searchForm.submit()
+        })
+    }
+}
+
+let tags = document.getElementsByClassName('project-tag')
+
+for(let i = 0; tags.length > i; i++){
+    tags[i].addEventListener('click', (e) => {
+        let tagId = e.target.dataset.tag
+        let projectId = e.target.dataset.project
+
+        fetch('http://127.0.0.1:8000/api/remove-tag/', {
+            method : 'DELETE',
+            headers : {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({ 'project': projectId, 'tag': tagId })
+        })
+            .then(response => response.json())
+            .then(data => {
+                e.target.remove()
+            })
+
+    })
+>>>>>>> 7843aec9eeba2f96c72e0160686042875a3d5fad
 }
